6340507b6b386bcf251de24e2a6a794b6de0d8aa
Afunsho Olamide <abolamide77@gmail.com>
2026-04-08 18:43:00 +0100

    polished the landing page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -101,7 +101,12 @@ const Navbar = () => {
                 {/* Mobile Menu */}
 
                 <AnimatePresence>
-                    <motion.div className={`fixed top-0 left-0 w-full h-screen bg-white/10 backdrop-blur-sm text-base flex flex-col lg:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                    <motion.div
+                        initial={{ x: "-100%" }}
+                        animate={{ x: 0 }}
+                        exit={{ x: "-100%" }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }} 
+                    className={`fixed top-0 left-0 w-full h-screen bg-white/10 backdrop-blur-sm text-base flex flex-col lg:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-5 sm:top-8 right-4 md:right-8 cursor-pointer" onClick={() => setIsMenuOpen(false)}>
                         <Image src={closeMenu} alt="Close Menu" width={20} height={20} />
                     </button>
