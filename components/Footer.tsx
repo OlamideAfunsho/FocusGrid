d40c01794cd973b8cf9621a2503870ae743a572e
Afunsho Olamide <abolamide77@gmail.com>
2026-04-08 16:27:35 +0100

    polished the landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,9 @@ import instagramIcon from '../public/images/landing_page_images/instagram-icon.s
 const Footer = () => {
   return (
     <>
-    <section className='bg-[#111827] px-4 py-12 sm:p-12 lg:px-[120px] lg:py-16'>
-        <section className='flex flex-col gap-12 md:flex-row justify-between'>
-            <div className='w-3/4 lg:w-2/5'>
+    <section className='bg-[#111827] px-4 py-8 sm:p-12 lg:px-[120px] lg:py-16'>
+        <section className='flex flex-col gap-8 md:gap-12 md:flex-row justify-between'>
+            <div className='w-full lg:w-2/5'>
                 <div className="flex items-center gap-2">
                     <Image src={logo} alt="FocusGrid Logo" />
                     <h1 className={`font-semibold text-[18px] sm:text-[24px] text-[#FFFFFF] `}>FocusGrid</h1>
@@ -19,7 +19,7 @@ const Footer = () => {
                 <p className='text-[16px] md:text-[20px] leading-[30px] md:leading-9 text-[#8795A3] mt-4'>The ultimate productivity platform built specifically for students. Organize your academic life, stay focused, and achieve your goals.</p>
             </div>
 
-            <div className='flex flex-col gap-6'>
+            <div className='hidden md:flex flex-col gap-6'>
                 <span className='text-[16px] sm:text-[20px] text-[#FFFFFF] font-semibold'>Product</span>
                 <ul className='flex flex-col gap-4'>
                     <li><span className='text-[14px] sm:text-[16px] text-[#8795A3] hover:text-[#3399FF] cursor-grab'>Features</span></li>
@@ -29,7 +29,7 @@ const Footer = () => {
                 </ul>
             </div>
             
-            <div className='flex flex-col gap-6'>
+            <div className='hidden md:flex flex-col gap-6'>
                 <span className='text-[16px] sm:text-[20px] text-[#FFFFFF] font-semibold'>Company</span>
                 <ul className='flex flex-col gap-4'>
                     <li><span className='text-[14px] sm:text-[16px] text-[#8795A3] hover:text-[#3399FF] cursor-grab'>About</span></li>
@@ -39,7 +39,7 @@ const Footer = () => {
                 </ul>
             </div>
 
-            <div className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-4 md:gap-6'>
                 <span className='text-[16px] sm:text-[20px] text-[#FFFFFF] font-semibold'>Support</span>
                 <ul className='flex flex-col gap-4'>
                     <li><span className='text-[14px] sm:text-[16px] text-[#8795A3] hover:text-[#3399FF] cursor-grab'>Help Center</span></li>
@@ -57,7 +57,7 @@ const Footer = () => {
             <Image src={facebookIcon} alt="Facebook" className='cursor-pointer' />
         </div>
 
-        <div className='flex gap-1 flex-col sm:flex-row justify-between sm:items-center border-t-2 pt-4 border-[#D9D8D80D] mt-12'>
+        <div className='flex gap-1 flex-col sm:flex-row justify-between sm:items-center border-t-2 pt-4 border-[#D9D8D80D] mt-8 md:mt-12'>
             <p className='text-[14px] text-[#8795A3]'>&copy; 2026 FocusGrid. All rights reserved.</p>
             <div>
                 <span className='text-[14px] text-[#8795A3] hover:text-[#3399FF] cursor-grab'>Privacy Policy</span>
