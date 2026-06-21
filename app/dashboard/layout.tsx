7350262d350877d739e325f52acd0c1ab8005860
Afunsho Olamide <abolamide77@gmail.com>
2026-06-21 11:21:17 +0100

    polished the dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,18 +3,24 @@ import SideBar from "./components/SideBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen w-screen flex-col-reverse md:flex-row overflow-hidden">
-      <div className="w-full md:w-[276px] md:h-full p-2 md:px-4 md:py-6 flex-none border-r-3 border-r-[#F9F9F9]">
-        <SideBar />
-      </div>
+    <div className="flex h-dvh w-screen flex-col-reverse md:flex-row overflow-hidden unique-layout-root">
+  {/* Bottom Bar on Mobile / Side Bar on Desktop */}
+  <div className="w-full md:w-[276px] h-fit md:h-full p-2 md:px-4 md:py-6 flex-shrink-0 md:flex-none border-t border-t-neutral-100 md:border-t-0 md:border-r-3 md:border-r-[#F9F9F9] bg-white z-10">
+    <SideBar />
+  </div>
 
-      <div className="flex flex-1 flex-col h-full overflow-hidden">
-        <div className=" w-full h-fit px-[30px] py-4 flex-none border-b-3 border-b-[#F9F9F9]">
-          <NavBar />
-        </div>
-
-        <div className="flex-1 overflow-y-auto px-[30px] py-5 bg-[#F9FAFB]">{children}</div>
-      </div>
+  {/* Main Workspace Column */}
+  <div className="flex flex-1 flex-col min-h-0 overflow-hidden">
+    {/* Top Nav Bar */}
+    <div className="w-full h-fit px-[30px] py-4 flex-shrink-0 md:flex-none border-b-3 border-b-[#F9F9F9] bg-white z-10">
+      <NavBar />
     </div>
+
+    {/* Scrollable Page View */}
+    <main className="flex-1 overflow-y-auto px-[30px] py-5 bg-[#F9FAFB]">
+      {children}
+    </main>
+  </div>
+</div>
   );
 }
