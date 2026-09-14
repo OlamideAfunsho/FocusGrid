81a106c8cbdeb6b7c740db1fccf371b901fdedf4
Afunsho Olamide <abolamide77@gmail.com>
2026-09-14 07:02:36 +0100

    feat: updated the middleware file by getting rid of the frontendAPiProxy configuration.

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware(
-  async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect();
-  },
-  {
-    frontendApiProxy: {
-      enabled: true,
-    },
-  }
-);
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
@@ -19,7 +12,5 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
-    // Explicitly handle all Clerk proxy requests (overrides static .js exclusions)
-    "/__clerk/(.*)",
   ],
 };
